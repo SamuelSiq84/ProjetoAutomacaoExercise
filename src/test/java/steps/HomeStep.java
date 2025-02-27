2dba57b539d5bdbd843eebee7d9435e633a2abbf
samuel <samuel.siqueira.pereira@gmail.com>
2025-02-27 17:19:23 -0300

    printls de cada passo de execução do teste

--- src/test/java/steps/HomeStep.java
+++ src/test/java/steps/HomeStep.java
@@ -4,6 +4,7 @@ import maps.HomeMap;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.sql.SQLOutput;
 
 
 public class HomeStep extends HomeMap {
@@ -13,16 +14,20 @@ public class HomeStep extends HomeMap {
     HomeMap homeMap = new HomeMap(driver);
 
     public HomeStep acessarURL(String url){
+        System.out.println("Acessar a URL");
     driver.get(url);
     return this;
     }
     public HomeStep acessarLoginPage(){
+        System.out.println("Acessando Login na Home Page");
     btnLoginHome.click();
     return this;
     }
     public HomeStep loginHome(String email, String password){
+        System.out.println("Preenchendo email: " + email + "e password: " + password);
     campoEmail.sendKeys(email);
     campoPassword.sendKeys(password);
+        System.out.println("Clicando no botão submit do Login");
     btnSubmit.click();
     return this;
     }
@@ -31,6 +36,7 @@ public class HomeStep extends HomeMap {
         return this;
     }
     public HomeStep acessarHomeProducts(){
+        System.out.println("Acessando pela Home a products");
         btnProducts.click();
         return this;
     }
